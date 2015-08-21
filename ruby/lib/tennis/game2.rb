cf305fbf450db36c26f9e4ed616938ecd5dcb644
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-08-22 00:43:01 +0200

    Refactored Tennis::Game2 class: Declared readonly variables, Better variable and method names

--- ruby/lib/tennis/game2.rb
+++ ruby/lib/tennis/game2.rb
@@ -1,61 +1,63 @@
 module Tennis
   class Game2
-    def initialize(player1Name, player2Name)
-      @player1Name = player1Name
-      @player2Name = player2Name
-      @p1points = 0
-      @p2points = 0
+    attr_reader :first_player_name, :second_player_name, :first_player_points, :second_player_points
+
+    def initialize(first_player_name, second_player_name)
+      @first_player_name = first_player_name
+      @second_player_name = second_player_name
+      @first_player_points = 0
+      @second_player_points = 0
     end
 
-    def won_point(playerName)
-      if playerName == @player1Name
-        p1Score()
+    def won_point(player_name)
+      if player_name == first_player_name
+        first_player_score
       else
-        p2Score()
+        second_player_score
       end
     end
 
     def score
       result = ""
-      if (@p1points == @p2points and @p1points < 3)
-        if (@p1points==0)
+      if (first_player_points == second_player_points and first_player_points < 3)
+        if (first_player_points==0)
           result = "Love"
         end
-        if (@p1points==1)
+        if (first_player_points==1)
           result = "Fifteen"
         end
-        if (@p1points==2)
+        if (first_player_points==2)
           result = "Thirty"
         end
         result += "-All"
       end
-      if (@p1points==@p2points and @p1points>2)
+      if (first_player_points==second_player_points and first_player_points>2)
         result = "Deuce"
       end
 
       p1res = ""
       p2res = ""
-      if (@p1points > 0 and @p2points==0)
-        if (@p1points==1)
+      if (first_player_points > 0 and second_player_points==0)
+        if (first_player_points==1)
           p1res = "Fifteen"
         end
-        if (@p1points==2)
+        if (first_player_points==2)
           p1res = "Thirty"
         end
-        if (@p1points==3)
+        if (first_player_points==3)
           p1res = "Forty"
         end
         p2res = "Love"
         result = p1res + "-" + p2res
       end
-      if (@p2points > 0 and @p1points==0)
-        if (@p2points==1)
+      if (second_player_points > 0 and first_player_points==0)
+        if (second_player_points==1)
           p2res = "Fifteen"
         end
-        if (@p2points==2)
+        if (second_player_points==2)
           p2res = "Thirty"
         end
-        if (@p2points==3)
+        if (second_player_points==3)
           p2res = "Forty"
         end
 
@@ -63,69 +65,69 @@ module Tennis
         result = p1res + "-" + p2res
       end
 
-      if (@p1points>@p2points and @p1points < 4)
-        if (@p1points==2)
+      if (first_player_points>second_player_points and first_player_points < 4)
+        if (first_player_points==2)
           p1res="Thirty"
         end
-        if (@p1points==3)
+        if (first_player_points==3)
           p1res="Forty"
         end
-        if (@p2points==1)
+        if (second_player_points==1)
           p2res="Fifteen"
         end
-        if (@p2points==2)
+        if (second_player_points==2)
           p2res="Thirty"
         end
         result = p1res + "-" + p2res
       end
-      if (@p2points>@p1points and @p2points < 4)
-        if (@p2points==2)
+      if (second_player_points>first_player_points and second_player_points < 4)
+        if (second_player_points==2)
           p2res="Thirty"
         end
-        if (@p2points==3)
+        if (second_player_points==3)
           p2res="Forty"
         end
-        if (@p1points==1)
+        if (first_player_points==1)
           p1res="Fifteen"
         end
-        if (@p1points==2)
+        if (first_player_points==2)
           p1res="Thirty"
         end
         result = p1res + "-" + p2res
       end
-      if (@p1points > @p2points and @p2points >= 3)
-        result = "Advantage " + @player1Name
+      if (first_player_points > second_player_points and second_player_points >= 3)
+        result = "Advantage " + first_player_name
       end
-      if (@p2points > @p1points and @p1points >= 3)
-        result = "Advantage " + @player2Name
+      if (second_player_points > first_player_points and first_player_points >= 3)
+        result = "Advantage " + @second_player_name
       end
-      if (@p1points>=4 and @p2points>=0 and (@p1points-@p2points)>=2)
-        result = "Win for " + @player1Name
+      if (first_player_points>=4 and second_player_points>=0 and (first_player_points-second_player_points)>=2)
+        result = "Win for " + first_player_name
       end
-      if (@p2points>=4 and @p1points>=0 and (@p2points-@p1points)>=2)
-        result = "Win for " + @player2Name
+      if (second_player_points>=4 and first_player_points>=0 and (second_player_points-first_player_points)>=2)
+        result = "Win for " + @second_player_name
       end
       result
     end
 
     def setp1Score(number)
       (0..number).each do |i|
-        p1Score()
+        first_player_score
       end
     end
 
     def setp2Score(number)
       (0..number).each do |i|
-        p2Score()
+        second_player_score
       end
     end
 
-    def p1Score
-      @p1points +=1
+    def first_player_score
+      @first_player_points +=1
     end
 
-    def p2Score
-      @p2points +=1
+    def second_player_score
+      @second_player_points +=1
     end
   end
 end
