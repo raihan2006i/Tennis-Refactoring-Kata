0540ea30d52c28137a403f079cea6df2902bbab5
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-08-22 00:23:05 +0200

    Refactored Tennis::Game1 class: Cleaning up redundant and unnecessary code

--- ruby/lib/tennis/game1.rb
+++ ruby/lib/tennis/game1.rb
@@ -10,32 +10,22 @@ module Tennis
     end
 
     def score
-      result = ''
       if first_player_points == second_player_points
-        result = POINTS_DESCRIPTION[:equal].fetch(first_player_points, 'Deuce')
+        POINTS_DESCRIPTION[:equal].fetch(first_player_points, 'Deuce')
       elsif first_player_points >= 4 || second_player_points >= 4
         points_difference = first_player_points - second_player_points
         if points_difference == 1
-          result = "Advantage #{first_player_name}"
+          "Advantage #{first_player_name}"
         elsif points_difference == -1
-          result = "Advantage #{second_player_name}"
+          "Advantage #{second_player_name}"
         elsif points_difference >= 2
-          result = "Win for #{first_player_name}"
+          "Win for #{first_player_name}"
         else
-          result = "Win for #{second_player_name}"
+          "Win for #{second_player_name}"
         end
       else
-        (1...3).each do |i|
-          if i == 1
-            temp_score = first_player_points
-          else
-            result += '-'
-            temp_score = second_player_points
-          end
-          result += POINTS_DESCRIPTION[:different].fetch(temp_score)
-        end
+        "#{POINTS_DESCRIPTION[:different].fetch(first_player_points)}-#{POINTS_DESCRIPTION[:different].fetch(second_player_points)}"
       end
-      result
     end
 
     def won_point(player_name)
