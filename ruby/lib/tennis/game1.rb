69739685db495fc10a27802ebd44e9a65369add6
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-08-21 20:11:36 +0200

    Refactored Tennis::Game1 class: Declared readonly variables, Better spacing

--- ruby/lib/tennis/game1.rb
+++ ruby/lib/tennis/game1.rb
@@ -1,5 +1,7 @@
 module Tennis
   class Game1
+    attr_reader :first_player_name, :second_player_name, :first_player_points, :second_player_points
+
     def initialize(first_player_name, second_player_name)
       @first_player_name = first_player_name
       @second_player_name = second_player_name
@@ -7,41 +9,33 @@ module Tennis
       @second_player_points = 0
     end
 
-    def won_point(player_name)
-      if player_name == @first_player_name
-        @first_player_points += 1
-      else
-        @second_player_points += 1
-      end
-    end
-
     def score
       result = ""
-      temp_score=0
-      if (@first_player_points==@second_player_points)
+      temp_score = 0
+      if first_player_points == second_player_points
         result = {
             0 => "Love-All",
             1 => "Fifteen-All",
             2 => "Thirty-All",
-        }.fetch(@first_player_points, "Deuce")
-      elsif (@first_player_points>=4 or @second_player_points>=4)
-        minus_result = @first_player_points-@second_player_points
-        if (minus_result==1)
-          result ="Advantage " + @first_player_name
-        elsif (minus_result ==-1)
-          result ="Advantage " + @second_player_name
-        elsif (minus_result>=2)
-          result = "Win for " + @first_player_name
+        }.fetch(first_player_points, "Deuce")
+      elsif first_player_points >= 4 or second_player_points >= 4
+        minus_result = first_player_points - second_player_points
+        if minus_result == 1
+          result ="Advantage " + first_player_name
+        elsif minus_result == -1
+          result ="Advantage " + second_player_name
+        elsif minus_result >= 2
+          result = "Win for " + first_player_name
         else
-          result ="Win for " + @second_player_name
+          result ="Win for " + second_player_name
         end
       else
         (1...3).each do |i|
-          if (i==1)
-            temp_score = @first_player_points
+          if i == 1
+            temp_score = first_player_points
           else
-            result+="-"
-            temp_score = @second_player_points
+            result += "-"
+            temp_score = second_player_points
           end
           result += {
               0 => "Love",
@@ -52,6 +46,14 @@ module Tennis
         end
       end
       result
+    end
+
+    def won_point(player_name)
+      if player_name == first_player_name
+        @first_player_points += 1
+      else
+        @second_player_points += 1
+      end
     end
   end
 end
