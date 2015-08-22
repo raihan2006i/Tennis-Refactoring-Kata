e8fed2e12022b049ed2c38d787f46ddbb548a3c4
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-08-22 14:54:25 +0200

    Refactored Tennis::Game3 class: Cleanups and some private methods

--- ruby/lib/tennis/game3.rb
+++ ruby/lib/tennis/game3.rb
@@ -11,25 +11,37 @@ module Tennis
 
     def score
       if (first_player_points < 4 && second_player_points < 4) && (first_player_points + second_player_points < 6)
-        points = %w(Love Fifteen Thirty Forty)
-        score = points[first_player_points]
-        first_player_points == second_player_points ? score + '-All' : score + '-' + points[second_player_points]
+        first_player_points == second_player_points ? "#{humanize_points[first_player_points]}-All" : "#{humanize_points[first_player_points]}-#{humanize_points[second_player_points]}"
       else
         if first_player_points == second_player_points
           'Deuce'
         else
           player_in_advantage = first_player_points > second_player_points ? first_player_name : second_player_name
-          (first_player_points - second_player_points) * (first_player_points - second_player_points) == 1 ? "Advantage #{player_in_advantage}" : "Win for #{player_in_advantage}"
+          points_difference * points_difference == 1 ? "Advantage #{player_in_advantage}" : "Win for #{player_in_advantage}"
         end
       end
     end
 
     def won_point(player_name)
-      if player_name == first_player_name
-        @first_player_points += 1
-      else
-        @second_player_points += 1
-      end
+      player_name == first_player_name ? first_player_won_point : second_player_won_point
+    end
+
+    private
+
+    def first_player_won_point
+      @first_player_points += 1
+    end
+
+    def humanize_points
+      %w(Love Fifteen Thirty Forty)
+    end
+
+    def points_difference
+      @points_difference ||= first_player_points - second_player_points
+    end
+
+    def second_player_won_point
+      @second_player_points += 1
     end
   end
 end
