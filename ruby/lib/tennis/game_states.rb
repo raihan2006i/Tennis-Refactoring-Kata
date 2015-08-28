d7153e35ef9827074e670b3d57b8c45e723f3023
Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-08-28 16:38:33 +0200

    Now we can introduce Fifty as a description for score 4 and change game rules easily

--- ruby/lib/tennis/game_states.rb
+++ ruby/lib/tennis/game_states.rb
@@ -6,12 +6,13 @@ require_relative 'game_states/win'
 module Tennis
   MIN_POINTS_DIFFERENCE_TO_WIN = 2
   MIN_POINTS_TO_WIN = 4
-  MIN_POINTS_TO_DEUCE = 3
+  MIN_POINTS_TO_DEUCE = 4
 
   POINTS_DESCRIPTION = {
       0 => 'Love',
       1 => 'Fifteen',
       2 => 'Thirty',
-      3 => 'Forty'
+      3 => 'Forty',
+      4 => 'Fifty'
   }
 end
--- ruby/test/test_game2.rb
+++ ruby/test/test_game2.rb
@@ -1,7 +1,52 @@
 require 'test/unit'
 require_relative '../lib/tennis/game2'
 require_relative 'supports/test_helpers'
-require_relative 'supports/test_cases'
+
+TEST_CASES = [
+    [0, 0, 'Love-All', 'player1', 'player2'],
+    [1, 1, 'Fifteen-All', 'player1', 'player2'],
+    [2, 2, 'Thirty-All', 'player1', 'player2'],
+    [3, 3, 'Forty-All', 'player1', 'player2'],
+    [4, 4, 'Deuce', 'player1', 'player2'],
+
+    [1, 0, 'Fifteen-Love', 'player1', 'player2'],
+    [0, 1, 'Love-Fifteen', 'player1', 'player2'],
+    [2, 0, 'Thirty-Love', 'player1', 'player2'],
+    [0, 2, 'Love-Thirty', 'player1', 'player2'],
+    [3, 0, 'Forty-Love', 'player1', 'player2'],
+    [0, 3, 'Love-Forty', 'player1', 'player2'],
+    [4, 0, 'Win for player1', 'player1', 'player2'],
+    [0, 4, 'Win for player2', 'player1', 'player2'],
+
+    [2, 1, 'Thirty-Fifteen', 'player1', 'player2'],
+    [1, 2, 'Fifteen-Thirty', 'player1', 'player2'],
+    [3, 1, 'Forty-Fifteen', 'player1', 'player2'],
+    [1, 3, 'Fifteen-Forty', 'player1', 'player2'],
+    [4, 1, 'Win for player1', 'player1', 'player2'],
+    [1, 4, 'Win for player2', 'player1', 'player2'],
+
+    [3, 2, 'Forty-Thirty', 'player1', 'player2'],
+    [2, 3, 'Thirty-Forty', 'player1', 'player2'],
+    [4, 2, 'Win for player1', 'player1', 'player2'],
+    [2, 4, 'Win for player2', 'player1', 'player2'],
+
+    [4, 3, 'Fifty-Forty', 'player1', 'player2'],
+    [3, 4, 'Forty-Fifty', 'player1', 'player2'],
+    [5, 4, 'Advantage player1', 'player1', 'player2'],
+    [4, 5, 'Advantage player2', 'player1', 'player2'],
+    [15, 14, 'Advantage player1', 'player1', 'player2'],
+    [14, 15, 'Advantage player2', 'player1', 'player2'],
+
+    [6, 4, 'Win for player1', 'player1', 'player2'],
+    [4, 6, 'Win for player2', 'player1', 'player2'],
+    [16, 14, 'Win for player1', 'player1', 'player2'],
+    [14, 16, 'Win for player2', 'player1', 'player2'],
+
+    [6, 4, 'Win for One', 'One', 'player2'],
+    [4, 6, 'Win for Two', 'player1', 'Two'],
+    [6, 5, 'Advantage One', 'One', 'player2'],
+    [5, 6, 'Advantage Two', 'player1', 'Two']
+]
 
 class TestGame2 < Test::Unit::TestCase
   def test_score
